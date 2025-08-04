b3dcfd9c52d5ba3e92ce9a175699b6a471aaf4db
Parvaggarawal01 <parvaggarwal130@gmail.com>
2025-08-05 02:44:46 +0530

    Signup Endpoint and User Schema Created

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,46 @@
 import express from "express";
 import mongoose from "mongoose";
 import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+import dotenv from "dotenv";
+import cors from "cors";
+import { UserModel } from "./db.js";
 const app = express();
 
-app.post("/api/v1/signup", (req, res) => {
+app.post("/api/v1/signup", async(req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
 
+  if(!username || !password) {
+    return res.status(401).json({
+      message: "Username and password are required"
+    })
+  }
+
+  try {
+    const salt = await bcrypt.genSalt(5);
+    const hashedPasseword = await bcrypt.hash(password, salt);
+
+    await UserModel.create({
+      username: username,
+      password: hashedPasseword
+    })
+
+    return res.status(200).json({
+      message: "Successfully created user"
+    })
+  }catch(err: any){
+    if(err.code === 11000){
+      return res.status(403).json({
+        message: "User Already Exists"
+      })
+    }
+
+    return res.status(500).json({
+      message: "Internal Server Error",
+      error: err.message
+    })
+  }
 })
 
 app.post("/api/v1/signin", (req, res) => {

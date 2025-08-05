e144f910cb8ff792d7b9b1a9f0716cdfddc1081c
Parvaggarawal01 <parvaggarwal130@gmail.com>
2025-08-05 16:52:51 +0530

    Adding Signin endpoint and content schema

--- src/db.ts
+++ src/db.ts
@@ -1,8 +1,26 @@
-import {model, Schema} from "mongoose";
+import mongoose, { model, Schema } from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+const MongoURL = process.env.MONGO_URL || "";
+
+mongoose
+  .connect(MongoURL)
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((error) => console.error("MongoDB connection error:", error));
 
 const UserSchema = new Schema({
-  username: {type: String, unique: true},
-  password: String
-})
+  username: { type: String, unique: true },
+  password: String,
+});
+
+const ContentSchema = new Schema({
+  title: String,
+  link: String,
+  tags: [{ type: mongoose.Types.ObjectId, ref: "Tag" }],
+  type: String,
+  userId: [{ type: mongoose.Types.ObjectId, ref: "User", required: true }],
+});
 
 export const UserModel = model("User", UserSchema);
+export const ContentModel = model("Content", ContentSchema);
--- src/index.ts
+++ src/index.ts
@@ -5,16 +5,21 @@ import bcrypt from "bcrypt";
 import dotenv from "dotenv";
 import cors from "cors";
 import { UserModel } from "./db.js";
-const app = express();
 
-app.post("/api/v1/signup", async(req, res) => {
+const app = express();
+app.use(express.json());
+app.use(cors());
+dotenv.config();
+const JWT_SECRET = process.env.JWT_SECRET || "";
+
+app.post("/api/v1/signup", async (req, res) => {
   const username = req.body.username;
   const password = req.body.password;
 
-  if(!username || !password) {
+  if (!username || !password) {
     return res.status(401).json({
-      message: "Username and password are required"
-    })
+      message: "Username and password are required",
+    });
   }
 
   try {
@@ -23,46 +28,91 @@ app.post("/api/v1/signup", async(req, res) => {
 
     await UserModel.create({
       username: username,
-      password: hashedPasseword
-    })
+      password: hashedPasseword,
+    });
 
     return res.status(200).json({
-      message: "Successfully created user"
-    })
-  }catch(err: any){
-    if(err.code === 11000){
+      message: "Successfully created user",
+    });
+  } catch (err: any) {
+    if (err.code === 11000) {
       return res.status(403).json({
-        message: "User Already Exists"
-      })
+        message: "User Already Exists",
+      });
     }
 
     return res.status(500).json({
       message: "Internal Server Error",
-      error: err.message
-    })
+      error: err.message,
+    });
   }
-})
+});
 
-app.post("/api/v1/signin", (req, res) => {
+app.post("/api/v1/signin", async (req, res) => {
+  try {
+    const username = req.body.username;
+    const password = req.body.password;
 
-})
+    if (!username || !password) {
+      return res.status(401).json({
+        message: "Username and password are required",
+      });
+    }
+
+    const user = await UserModel.findOne({
+      username: username,
+    });
+
+    if (!user) {
+      return res.status(403).json({
+        message: "User does not exist",
+      });
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password!);
+
+    if (!isPasswordValid) {
+      return res.status(403).json({
+        message: "Invalid Password",
+      });
+    } else {
+      const token = jwt.sign(
+        {
+          id: user._id,
+          username: user.username,
+        },
+        JWT_SECRET
+      );
+
+      res.json({
+        message: "Login Successful",
+        token: token,
+      });
+    }
+  } catch (err) {
+    return res.status(500).json({
+      message: "Internal Server Error",
+      error: err,
+    });
+  }
+});
 
 app.post("/api/v1/content", (req, res) => {
+  const link = req.body.link;
+  const title = req.body.title;
 
-})
+  
+});
 
-app.get("/api/v1/content", (req, res) => {
+app.get("/api/v1/content", (req, res) => {});
 
-})
+app.delete("/api/v1/content", (req, res) => {});
 
-app.delete("api/v1/content", (req, res) => {
+app.post("/api/v1/brain/share", (req, res) => {});
 
-})
+app.get("/api/v1/brain/share", (req, res) => {});
 
-app.post("api/v1/brain/share", (req, res) => {
-
-})
-
-app.get("api/v1/brain/share", (req, res) => {
-
-})
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});

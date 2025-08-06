fc85a6332bfccf1bd51931ee46ab13d53254e92a
Parvaggarawal01 <parvaggarwal130@gmail.com>
2025-08-07 00:14:45 +0530

    Adding All the important Icons

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,13 +1,27 @@
-import {Button} from "./components/button"
+import { PlusIcon } from "./icons/PlusIcon";
+import { Button } from "./components/button";
+import { ShareIcon } from "./icons/ShareIcon";
+// import { EarthIcon } from "./icons/EarthIcon";
 
 function App() {
-
   return (
     <div>
-      <Button variants="primary" text="Hello" size="md" onClick={()=>{}}></Button>
-      <Button variants="secondary" text="Hello" size="md" onClick={()=>{}}></Button>
+      <Button
+        variants="secondary"
+        text="Share"
+        size="md"
+        startIcon={<PlusIcon size="lg" />}
+        onClick={() => {}}
+      ></Button>
+      <Button
+        variants="primary"
+        text="Add Content"
+        size="md"
+        startIcon={<ShareIcon size="lg" />}
+        onClick={() => {}}
+      ></Button>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;

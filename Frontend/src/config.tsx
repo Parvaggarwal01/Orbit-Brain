65f8c7237351815748f168596a7477b964895982
Parvaggarawal01 <parvaggarwal130@gmail.com>
2025-08-07 23:34:19 +0530

    deployment

--- Frontend/src/config.tsx
+++ Frontend/src/config.tsx
@@ -1,2 +1,2 @@
-// Update this URL after Railway deployment
-export const BACKEND_URL = "http://localhost:10000"; // Change to your Railway URL after deployment
+
+export const BACKEND_URL = "http://localhost:10000"
